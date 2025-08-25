9183b1879b11e8f200de29c7cfb26a222380650d
mixmash <79651247+mixmash@users.noreply.github.com>
2025-08-25 16:48:49 +0200

    Refactored Project to be self-contained via uv

--- jsonlz4_conv.py
+++ jsonlz4_conv.py
@@ -1,4 +1,10 @@
-#!/usr/bin/env python3
+#!/usr/bin/env -S uv run --script
+# /// script
+# requires-python = ">=3.13"
+# dependencies = [
+#     "lz4>=4.0.2",
+# ]
+# ///
 import pathlib
 import os
 import argparse
